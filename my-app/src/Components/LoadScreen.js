e5948a3ca5bf0ef04b3ffa9f7df227528d601b5e
satyazzz123 <105061492+satyazzz123@users.noreply.github.com>
2023-01-29 02:23:35 +0530

    all new components created

--- my-app/src/Components/LoadScreen.js
+++ my-app/src/Components/LoadScreen.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function LoadScreen() {
+    const[show1,toShow1]=useState("block");
+    const[show2,toShow2]=useState("block")
+    const[show3,toShow3]=useState("block")
+  useEffect(()=>{
+        setTimeout(()=>{
+            toShow1("none")
+        },800);
+        setTimeout(()=>{
+            toShow2("none")
+        },1200);
+        setTimeout(()=>{
+            toShow3("none")
+        },1600)
+  },[])
   return (
     <div>
-      Hello gulu
+      <div className="load_body">
+        <span style={{padding:"0.20rem",display:`${show1}`}}>Satyajit </span> <span style={{padding:"0.20rem",display:`${show2}`}}>Behera's </span><span style={{padding:"0.20rem",display:`${show3}`}}> Portfolio</span>
+      </div>
     </div>
   )
 }
