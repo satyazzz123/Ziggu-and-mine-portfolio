4d6eb6e919680dbe4a9ee9b16e25f55118957b52
satyazzz123 <105061492+satyazzz123@users.noreply.github.com>
2023-01-29 18:02:07 +0530

    new changes to design

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div>
       
-        home
+        <div className="Home">
+          <div className="overlay">
+            <h1 style={{fontSize:"3.2rem",}}>Satyajit Behera</h1>
+            <a href=""><h2>Projects</h2></a>
+            <a href=""><h2>About Me</h2></a>
+            <a href=""><h2>Contact Me</h2></a>
+          </div>
+        </div>
     
     </div>
   )
